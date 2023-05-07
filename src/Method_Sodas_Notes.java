95e70578cfda2baa8f61df0f6867ef48af28a7cf
JoleneMel <jolenemelanson33@gmail.com>
2023-05-07 17:00:12 -0400

    finsihing up on everything

--- src/Method_Sodas_Notes.java
+++ src/Method_Sodas_Notes.java
@@ -1,9 +1,50 @@
 
 public class Method_Sodas_Notes {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	//The main method is where our tests for our methods will go 
+		public static void main(String[] args) {
+			//Soda = parameter
+			//Cup = method 
+			
+			//Soda is a vague name for what we want in it, in Java you do have to specify what data type you will be using 
+			//in this instance we will use a String 
+			
+			//Cup is like the function because it is taking in the soda aka parameter inside and serving it. 
+			
+			//soda is the parameter, but something like "Cola", "Root beer", etc are arguments, you can remember this because 
+			//people may argue about what is the best soda flavor. 
+			
+			//For returns we have to add a sysout for it to show on the console properly 
+		
+		}
+		//This is like someone telling you what the soda they would like to have 
+		
+		
+		
+		//Many programmers start with accidentally setting their parameter 
+		
+		//This sets the output to only print out one outcome, a method is supposed to be a tool or like a multi-purpose solution 
+		
+		
+		//Multiple parameters? We are gonna go to a fancy soda place so they have flavors~ 
+		
+		//example where this is also set 
+		
+		
+		//A void returns nothing, it stops within the console, so it doesnt have any data to pass on to anything else 
+		//so below are some examples of methods that return data and the benefits of that 
+		
+		
+		
+		//Example of it being set too 
+		
+		
+		
+		//With returns we can pass on the data to different methods to add to them. 
+		
+		
+		
+		//So with returns we will call the previously made methods to make it easier for us :) 
+		
+		
 }
