d9672fde5d35061440d88034ae4d2d5b42205d5b
JoleneMel <jolenemelanson33@gmail.com>
2023-05-07 16:21:30 -0400

    pushing method sodas

--- src/Method_Sodas.java
+++ src/Method_Sodas.java
@@ -5,6 +5,17 @@ public class Method_Sodas {
 		// TODO Auto-generated method stub
 		//Soda = parameter
 		//Cup = method 
+		
+		
+		//Soda is a vague name for what we want in it, in Java you do have to specify what 
+		
 	}
+	//This is like someone telling you what the soda they would like to have 
+	public static void sodaOrder(String soda) {
+		System.out.println("I would like a " + soda);
+	}
+	
+	
+	//Many programmers start with accidentally setting their parameter 
 
 }
