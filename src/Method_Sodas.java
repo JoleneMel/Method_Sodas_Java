96fe660af0d819aa9318d68d25823aacf0f7eee3
JoleneMel <jolenemelanson33@gmail.com>
2023-05-07 16:24:44 -0400

    adding comments

--- src/Method_Sodas.java
+++ src/Method_Sodas.java
@@ -1,13 +1,14 @@
 
 public class Method_Sodas {
 
+	//The main method is where our tests for our methods will go 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//Soda = parameter
 		//Cup = method 
 		
 		
-		//Soda is a vague name for what we want in it, in Java you do have to specify what 
+		//Soda is a vague name for what we want in it, in Java you do have to specify what data type you will be using 
+		//in this instance we will use a String 
 		
 	}
 	//This is like someone telling you what the soda they would like to have 
@@ -17,5 +18,13 @@ public class Method_Sodas {
 	
 	
 	//Many programmers start with accidentally setting their parameter 
-
+	public static void setSodaOrder(String soda) {
+		soda = "Cola";
+		System.out.println("I would like a " + soda);
+	}
+	//This sets the output to only print out one outcome, a method is supposed to be a tool or like a multipurpose solution 
+	
+	
+	
+	
 }
